9f3c1b3ba0351edb34544260078658d2cf742ee5
Ariel Arias <ariel.arias.tipan14@gmail.com>
2025-08-18 10:32:03 -0500

    guia25 terminada

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,9 +1,10 @@
 from django.conf import settings
 import requests
 from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required, permission_required
 
 @login_required
+@permission_required('dashboard.index_viewer', raise_exception=True)
 def index(request):
     api_url = "https://arielarias.pythonanywhere.com/demo/rest/api/index/"
     
